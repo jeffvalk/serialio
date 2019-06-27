670bd354a534bc80b3ceb4ac0c257f92acfcd5cf
Jeff Valk <jv@jeffvalk.com>
2019-06-27 14:29:29 -0400

    Update version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject serialio "0.2.0"
+(defproject serialio "0.2.1"
   :description "A flexible serial port library"
   :url "http://github.com/jeffvalk/serialio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [rxtx22 "1.0.6"]])
