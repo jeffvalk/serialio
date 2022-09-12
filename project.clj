f717025a309884f88167d7ffc83d284f97554b26
Jeff Valk <jv@jeffvalk.com>
2022-09-12 16:59:16 -0400

    Remove hard clojure dependency and bump version to 0.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject serialio "0.2.1"
+(defproject serialio "0.3.0"
   :description "A flexible serial port library"
   :url "http://github.com/jeffvalk/serialio"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.github.purejavacomm/purejavacomm "1.0.2.RELEASE"]])
+  :dependencies [[com.github.purejavacomm/purejavacomm "1.0.2.RELEASE"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]]}})
